9ed35cc7fface8c242827056118c34601df72fb9
Antony Muga <antonymuga@users.noreply.github.com>
2018-08-17 16:30:12 +0300

    Added Spanish translations for Sheng words that start with A

--- Dictionaries/ToSpa.py
+++ Dictionaries/ToSpa.py
@@ -1,5 +1,5 @@
 #	++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#	*        RUNTIME SHENG TRANSLATOR - ENGLISH TRACK        *
+#	*        RUNTIME SHENG TRANSLATOR - SPANISH TRACK        *
 #	++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #	*  Welcome to Runtime Sheng translator. v1.0.0           *
 #	*  MIT License, Copyright(c) 2018, Antony Muga           *
@@ -19,6 +19,69 @@
 #	*  Email: https://sites.google.com/view/antonymuga/home  *
 #	----------------------------------------------------------
 
-# Below is a dummy dictionary to test if the core RuntimeTranslate core logic works
+# Only has sheng words that start with letter A for now
 
-shengToSpa = {"Manight":"Buenos Noches", "Mdhii": "Person", "Wode": "Water", "Lapi": "Laptop", "Mbota": "Watch", "Ndai": "Car"}
+shengToSpa = {
+"A-Level" : "Sexo anal",
+"Aajabu ": "Asombroso",
+"Abdala" : "Pene",  
+"Abdalla ": "Pene",   
+"Achumbati" : "Una mujer",   
+"Achwiti" : "Una mujer",   
+"Adida" : "Fútbol",  
+"Adidaro ": "Adidas",  
+"Adisia" : "Añadir más",   
+"Advencha" : "Aventuras",  
+"Afere" : "Tarifa de autobús o Matatu",   
+"Afko" : "Alcohol",   
+"Agiza" : "Pedir algo",   
+"Agote" : "Dile hola a",  
+"Ahando ": "Un centenar",  
+"Air ponje" : "Aeropuerto",   
+"Akadai" : "Él / ella reclamó",  
+"Akajam ": "Él / ella se enojó",   
+"Akajangori" : "Él / ella se enojó",  
+"Akala" : "Sandalias hechas de neumáticos viejos",  
+"Akamero" : "Camión de la basura",   
+"Akorino" : "Policía de transito",   
+"Alaji" : "Por lo general, un hombre con un estómago grande o una persona rica", 
+"Alidai" : "Él / ella reclamó",   
+"Aliningam" : "Él / ella me vio",  
+"Alipewa" : "Él / ella se hizo íntimo con alguien",  
+"Aliturn" : "Volvió",  
+"Alloy" : "Llantas de coche",   
+"Aluru" : "Codornices",  
+"Alutas" : "Miraa/khat",  
+"Ambaa ": "Salir",  
+"Amebeat" : "Él / ella no es atractivo",    
+"Amechapa" : "Él / ella no es atractivo",  
+"Amechoka" : "Él / ella no es atractivo",  
+"Ameparara" : "Él / ella no es atractivo",   
+"Amero ": "Estados Unidos de America", 
+"Amful" : "Cigarrillo",  
+"Anadai" : "Él / ella está reclamando", 
+"Ananedesha ": "Él / ella tiene un estómago corriendo",  
+"Angukia" : "Ocurrir sobre algo con carencia",
+"Ankara" : "Cannabis Sativa/Bhang",  
+"Ankules" : "Tío",   
+"Antelope" : "Una mujer hermosa",   
+"Antidhes" : "Tía",  
+"Antinyita" : "No entender algo",   
+"Arbesa" : "Cuarenta chelines",  
+"Area" : "Barrio",   
+"Arif" : "Un amigo o conocido",   
+"Arme" : "Un soldado",  
+"ARV" : "Licor falso o de segunda generación",   
+"Asapa" : "Aquí",     
+"Ashara" : "Negocio",   
+"Ashoo" : "Diez chelines",  
+"Ashu" : "Diez chelines",   
+"Atacho" : "Anexo industrial del estudiante",   
+"Ati" : "¿Qué?",  
+"Atmo" : "Atmósfera",  
+"Atoti" : "Una chica atractiva",  
+"Aunty" : "Househelp o mucama",  
+"Avunja" : "Botas",  
+"Azaara" : "Desastre o calamidad",  
+"Azaro" : "Una camisa"  
+}
