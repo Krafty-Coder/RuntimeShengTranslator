1d1b97803904e4694cd347de606b4b10d830cf1e
Antony Muga <antonymuga@users.noreply.github.com>
2018-08-17 16:27:57 +0300

    Added German translations for Sheng words that start with A

--- Dictionaries/ToGer.py
+++ Dictionaries/ToGer.py
@@ -1,5 +1,5 @@
 #	++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#	*        RUNTIME SHENG TRANSLATOR - ENGLISH TRACK        *
+#	*        RUNTIME SHENG TRANSLATOR - GERMAN  TRACK        *
 #	++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #	*  Welcome to Runtime Sheng translator. v1.0.0           *
 #	*  MIT License, Copyright(c) 2018, Antony Muga           *
@@ -19,6 +19,69 @@
 #	*  Email: https://sites.google.com/view/antonymuga/home  *
 #	----------------------------------------------------------
 
-# Below is a dummy dictionary to test if the core RuntimeTranslate core logic works
+# Only has sheng words that start with letter A for now
 
-shengToGer = {"Manight":"Van Night", "Mdhii": "Person", "Wode": "Water", "Lapi": "Laptop", "Mbota": "Watch", "Ndai": "Car"}
+shengToGer = {
+"A-Level" : "Analsex",
+"Aajabu ": "Tolle",
+"Abdala" : "Penis",  
+"Abdalla ": "Penis",   
+"Achumbati" : "Ein Mädchen",   
+"Achwiti" : "Ein Mädchen",   
+"Adida" : "Fußball oder Fußball",  
+"Adidaro ": "Adidas",  
+"Adisia" : "Fügen Sie mehr hinzu",   
+"Advencha" : "Abenteuer",  
+"Afere" : "Bus oder Matatu Tarif",   
+"Afko" : "Alkohol",   
+"Agiza" : "Frage nach etwas",   
+"Agote" : "Grüße",  
+"Ahando ": "Einhundert",  
+"Air ponje" : "Flughafen",   
+"Akadai" : "Er / sie behauptete",  
+"Akajam ": "Er / sie wurde wütend",   
+"Akajangori" : "Er / sie wurde wütend",  
+"Akala" : "Sandalen aus alten Reifen",  
+"Akamero" : "Müllwagen",   
+"Akorino" : "Verkehrspolizist",   
+"Alaji" : "Normalerweise ein Mann mit einem großen Magen oder reiche Person", 
+"Alidai" : "Er / sie behauptete",   
+"Aliningam" : "Er / sie hat mich gesehen",  
+"Alipewa" : "Er / sie hat sich mit jemandem vertraut gemacht",  
+"Aliturn" : "Zurückgegangen",  
+"Alloy" : "Autofelgen",   
+"Aluru" : "Wachteln",  
+"Alutas" : "Miraa/khat",  
+"Ambaa ": "Verlassen",  
+"Amebeat" : "Er / sie ist nicht attraktiv",    
+"Amechapa" : "Er / sie ist nicht attraktiv",  
+"Amechoka" : "Er / sie ist nicht attraktiv",  
+"Ameparara" : "Er / sie ist nicht attraktiv",   
+"Amero ": "vereinigte Staaten von Amerika", 
+"Amful" : "Zigarette",  
+"Anadai" : "Er / sie behauptet", 
+"Ananedesha ": "Er / sie hat einen laufenden Magen",  
+"Angukia" : "Pass auf etwas mit Mangel auf",
+"Ankara" : "Cannabis Sativa/Bhang",  
+"Ankules" : "Onkel",   
+"Antelope" : "Eine schöne Frau",   
+"Antidhes" : "Tante",  
+"Antinyita" : "Versäumnis etwas zu verstehen",   
+"Arbesa" : "Vierzig Schilling",  
+"Area" : "Gegend",   
+"Arif" : "Ein Freund oder ein Bekannter",   
+"Arme" : "Ein Soldat",   
+"ARV" : "Fälschung oder Likör der zweiten Generation",   
+"Asapa" : "Genau hier",     
+"Ashara" : "Geschäft",   
+"Ashoo" : "Zehn Schilling",  
+"Ashu" : "Zehn Schilling",   
+"Atacho" : "Industrieller Anhang des Studenten",   
+"Ati" : "Das?",  
+"Atmo" : "Atmosphäre",  
+"Atoti" : "Ein attraktives Mädchen",  
+"Aunty" : "Haushälterin oder Magd",  
+"Avunja" : "Stiefel",  
+"Azaara" : "Disaster or calamity",  
+"Azaro" : "Ein Hemd"  
+}
