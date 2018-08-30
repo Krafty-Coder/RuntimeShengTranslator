adf268e2fa74a66a47b8f85b0a5a2a404f08a10d
Antony Muga <antonymuga@users.noreply.github.com>
2018-08-30 12:19:34 +0300

    Added the reverse translation file, working on the Logic

---
+++ ReverseShengTranslate.py
@@ -0,0 +1,49 @@
+#	++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#	*        RUNTIME SHENG TRANSLATOR -  REVERSE DIRECTION   *
+#	++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#	*  Welcome to Runtime Sheng translator. v1.0.0           *
+#	*  MIT License, Copyright(c) 2018, Antony Muga           *
+#	*  All Rights Reserved.                                  *
+#	*  Author: Antony Muga                                   *
+#	----------------------------------------------------------
+#	*  Project's Links:                                      *
+#	*  Twitter: https://twitter.com/RuntimeLab               *
+#	*  Runtime Lab on LinkedIn                               *
+#	*  RuntimeLab on Github                                  *
+#	*  RuntimeShengTranslator project on GitHub              *
+#	----------------------------------------------------------
+#	*  Personal social links:                                *
+#	*  GitHub: https://github.com/antonymuga/                *
+#	*  Website: https://antonymuga.github.io                 *
+#	*  LinkedIn: https://www.linkedin.com/in/antony-muga/    *
+#	*  Email: https://sites.google.com/view/antonymuga/home  *
+#	----------------------------------------------------------
+# The below 'import sys' is currently vestigial now but will come in handy while writing new submissions
+# into the individual dictionary files, open, write, close and all that Jazz
+import sys
+
+# Import the project details
+import about
+
+# Import the dictionaries for the different languages
+from Dictionaries import ToEng # English dictionary
+from Dictionaries import ToFre # French dictionary
+from Dictionaries import ToGer # German dictionary
+from Dictionaries import ToSpa # Spanish dictionary
+from Dictionaries import ToIta # Italian dictionary
+
+# Print line separator
+print("\n","*"*75, "\n")
+
+# Print the project details
+print(about.projectDetails)
+
+# Print line separator for different sections
+print("\n","*"*75, "\n")
+
+
+# Declare a tuple that contains all the imported dictionaries
+dictionaryList = (ToEng.shengToEng, ToFre.shengToFre, ToGer.shengToGer, ToSpa.shengToSpa, ToIta.shengToIta)
+
+# Enter new word submissions
+submissions = []
